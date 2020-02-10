3ef7bf6e52fcbc8fb0ae0dc1d3295b1ba6257c6b
Jon Barclay <my@nameisjon.com>
2020-02-10 12:09:36 -0600

    sensor test

--- server.js
+++ server.js
@@ -1,29 +1,23 @@
 const express = require('express');
-// const { Bme680 } = require('bme680-sensor');
+const { Bme680 } = require('bme680-sensor');
 
 const app = express();
 const PORT = 5000;
 
-// const bme680 = new Bme680(1, 0x77);
+const bme680 = new Bme680(1, 0x77);
 
-// let bmeData = [];
+let bmeData = [];
 
-// bme680.initialize().then(async () => {
-//     console.log('Bme680 Sensor initialized');
-//     setInterval(async () => {
-//         // console.log(await bme680.getSensorData());
-//         bmeData.push(await bme680.getSensorData());
-//     }, 3000);
-// });
-
-app.get('/', (req, res) => {
-    res.send('hello world');
+bme680.initialize().then(async () => {
+    console.log('Bme680 Sensor initialized');
+    setInterval(async () => {
+        // console.log(await bme680.getSensorData());
+        bmeData.push(await bme680.getSensorData());
+    }, 3000);
 });
 
-app.get('/kill', (rrq, res) => {
-    res.send('closing...');
-    var server = app.listen( PORT );
-    server.close();
+app.get('/', (req, res) => {
+    res.send(JSON.stringify(bmeData, null, 2));
 });
 
 app.listen(PORT, () => {
